648b0571bfabd83bd2ca8080527ec0896ed84993
dimpap <dimpap5555@gmail.com>
2025-08-01 01:40:26 +0300

    Refactored code into objects

---
+++ blasius.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+class BlasiusFlow:
+    """Compute the steady Blasius boundary layer solution on a given grid."""
+
+    def __init__(self, U_inf, nu, x, y, eta_max=10.0, Neta=1000):
+        self.U_inf = U_inf
+        self.nu = nu
+        self.x = np.asarray(x)
+        self.y = np.asarray(y)
+        self.eta_max = eta_max
+        self.Neta = Neta
+        self.dx = self.x[1] - self.x[0]
+        self.dy = self.y[1] - self.y[0]
+        self._compute_similarity_solution()
+        self.U0, self.V0 = self._compute_base_flow()
+
+    def _compute_similarity_solution(self):
+        eta = np.linspace(0.0, self.eta_max, self.Neta)
+        f = np.zeros((3, self.Neta))
+        f[2, 0] = 0.332057336215194  # f''(0)
+        d_eta = eta[1] - eta[0]
+
+        def rhs(state):
+            f0, f1, f2 = state
+            return np.array([f1, f2, -0.5 * f0 * f2])
+
+        for k in range(self.Neta - 1):
+            s = f[:, k]
+            k1 = rhs(s)
+            k2 = rhs(s + 0.5 * d_eta * k1)
+            k3 = rhs(s + 0.5 * d_eta * k2)
+            k4 = rhs(s + d_eta * k3)
+            f[:, k + 1] = s + (d_eta / 6.0) * (k1 + 2*k2 + 2*k3 + k4)
+
+        self.eta = eta
+        self.f0_eta = f[0]
+        self.f1_eta = f[1]
+
+    def _compute_base_flow(self):
+        Nx = len(self.x)
+        Ny = len(self.y)
+        U0 = np.zeros((Ny, Nx))
+        V0 = np.zeros((Ny, Nx))
+        for i, xi in enumerate(self.x):
+            for j, yj in enumerate(self.y):
+                if xi <= 0:
+                    U0[j, i] = self.U_inf * self.f1_eta[-1]
+                    V0[j, i] = 0.0
+                else:
+                    et = yj * np.sqrt(self.U_inf / (self.nu * xi))
+                    idx = min(int(et / self.eta_max * (self.Neta - 1)), self.Neta - 1)
+                    up = self.f1_eta[idx]
+                    U0[j, i] = self.U_inf * up
+                    V0[j, i] = 0.5 * np.sqrt(self.nu * self.U_inf / xi) * (et * up - self.f0_eta[idx])
+        return U0, V0
+
+    def wall_shear_drag(self, mu, x):
+        dy = self.dy
+        dUdy = (self.U0[1, :] - self.U0[0, :]) / dy
+        tau_w = mu * dUdy
+        return np.trapezoid(tau_w, x)
---
+++ solver.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+class LinearBLSolver:
+    """Linearized boundary layer solver using a given Blasius base flow."""
+
+    def __init__(self, blasius_flow, rho, nu, dt, Nt, epsilon, omega):
+        self.base = blasius_flow
+        self.rho = rho
+        self.nu = nu
+        self.dt = dt
+        self.Nt = Nt
+        self.epsilon = epsilon
+        self.omega = omega
+
+        self.x = blasius_flow.x
+        self.y = blasius_flow.y
+        self.dx = self.x[1] - self.x[0]
+        self.dy = self.y[1] - self.y[0]
+        self.U0 = blasius_flow.U0
+        self.V0 = blasius_flow.V0
+        self.Nx = len(self.x)
+        self.Ny = len(self.y)
+        self.time = np.arange(Nt) * dt
+
+    def inlet_profile(self, t):
+        return self.epsilon * self.base.U_inf * np.sin(self.omega * t) * np.exp(-self.y / (0.1 * self.y[-1]))
+
+    def run(self):
+        delta = np.zeros((self.Ny, self.Nx))
+        frames_u = []
+        frames_v = []
+        for n, t in enumerate(self.time):
+            delta[:, 0] = self.inlet_profile(t)
+            new = delta.copy()
+            for i in range(1, self.Nx - 1):
+                for j in range(1, self.Ny - 1):
+                    dudx = (delta[j, i] - delta[j, i - 1]) / self.dx
+                    dudy = (delta[j, i] - delta[j - 1, i]) / self.dy
+                    adv = -self.U0[j, i] * dudx - self.V0[j, i] * dudy
+                    d2udx2 = (delta[j, i + 1] - 2 * delta[j, i] + delta[j, i - 1]) / self.dx ** 2
+                    d2udy2 = (delta[j + 1, i] - 2 * delta[j, i] + delta[j - 1, i]) / self.dy ** 2
+                    diff = self.nu * (d2udx2 + d2udy2)
+                    new[j, i] = delta[j, i] + self.dt * (adv + diff)
+            new[0, :] = 0.0
+            new[-1, :] = new[-2, :]
+            new[:, -1] = new[:, -2]
+            delta = new
+
+            vprime = np.zeros_like(delta)
+            for i in range(self.Nx):
+                for j in range(1, self.Ny):
+                    ddux = (delta[j, i] - delta[j - 1, i]) / self.dy
+                    vprime[j, i] = vprime[j - 1, i] - self.dx * ddux
+
+            u_abs = self.U0 + delta
+            v_abs = self.V0 + vprime
+            if n % 5 == 0:
+                frames_u.append(u_abs.copy())
+                frames_v.append(v_abs.copy())
+        frames_u = np.array(frames_u)
+        frames_v = np.array(frames_v)
+        frames_u = np.nan_to_num(frames_u, nan=0.0, posinf=0.0, neginf=0.0)
+        frames_v = np.nan_to_num(frames_v, nan=0.0, posinf=0.0, neginf=0.0)
+        nframes = frames_u.shape[0]
+        t_snap = np.arange(nframes) * 5 * self.dt
+        return frames_u, frames_v, t_snap
+
+    def compute_drag(self, frames_u, mu):
+        drag = np.zeros(frames_u.shape[0])
+        for k in range(frames_u.shape[0]):
+            dud_y = (frames_u[k, 1, :] - frames_u[k, 0, :]) / self.dy
+            drag[k] = mu * np.trapezoid(dud_y, self.x)
+        return np.nan_to_num(drag)
